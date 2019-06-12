7333e3e30ba9f1cb4a679be56720bbae18329b99
shishaodong <shishaodong@cmhi.chinamobile.com>
2019-06-12 16:40:45 +0800

    新增上传页面

--- src/main/java/com/github/springbootmonitor/config/ConfigCheckInfo.java
+++ src/main/java/com/github/springbootmonitor/config/ConfigCheckInfo.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "com.cmcc")
+@ConfigurationProperties(prefix = "com.github")
 public class ConfigCheckInfo {
 
     private Map<String, String> cdn;
