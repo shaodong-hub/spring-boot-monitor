3ae79f9e8488e601e17fafc3ac94f08cf2477fe8
JeenoD <jeenocruise@gmail.com>
2019-06-26 16:50:27 +0800

    错误提示信息整理

---
+++ src/main/java/com/github/springbootmonitor/common/ErrorMsgs.java
@@ -0,0 +1,20 @@
+package com.github.springbootmonitor.common;
+
+/**
+ * @Author: Du Jiahao
+ * @Date: 2019/6/26 0026 16:34
+ */
+public class ErrorMsgs {
+    // 文件内容相关
+    public static String CONTENT_HOST_NOT_VALID = "域名格式有误";
+    public static String CONTENT_SOURCE_IP_NOT_VALID = "源站地址有误";
+    public static String CONTENT_CDN_IP_NOT_VALID = "cdn地址有误";
+    public static String CONTENT_WAF_IP_NOT_VALID = "waf地址有误";
+    public static String CONTENT_HTTP_NOT_VALID = "是否http请求填写有误";
+    public static String CONTENT_DESC_NOT_VALID = "描述内容格式有误";
+    public static String CONTENT_LACK = "文件内容列有缺失";
+    public static String CONTENT_NOT_VALID = "文件格式有误";
+
+    // 文件操作相关
+    public static String IMPORT_CSV_FAILED = "导出csv文件失败";
+}
